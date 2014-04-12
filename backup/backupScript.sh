ec12fcd142747a8c1b353988202d1078f36d1eeb
Shruti <shruti@vinsol.com>
2014-04-12 14:31:38 +0530

    Removed empty lines

--- backup/backupScript.sh
+++ backup/backupScript.sh
@@ -3,4 +3,4 @@ path=`dirname $0`
 presentDate=`date +'%d_%m_%y_%H%M'`
 echo 'creating backup....'
 tar -czvf $path/backup_$presentDate.tar.gz $path/important
-echo 'done with backup'
+echo '--Backup Complete--'
