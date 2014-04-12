9dafea0ded56c3c0067b37569b281d27d03a2ea7
Shruti <shruti@vinsol.com>
2014-04-12 14:19:45 +0530

    Changed backupScript.sh

--- backup/backupScript.sh
+++ backup/backupScript.sh
@@ -2,5 +2,5 @@
 path=`dirname $0`
 presentDate=`date +'%d_%m_%y_%H%M'`
 echo 'creating backup....'
-command=`tar -czvf $path/backup_$presentDate.tar.gz $path/important`
+tar -czvf $path/backup_$presentDate.tar.gz $path/important
 echo 'done with backup'
