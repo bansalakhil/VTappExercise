43d3bd07aaffe4fa403b61836fd9130b705c32e6
Shruti <shruti@vinsol.com>
2014-04-15 18:05:32 +0530

    Report Generation

--- ReportGeneration/CreateUsers.sh
+++ ReportGeneration/CreateUsers.sh
@@ -2,13 +2,9 @@
 path=`dirname $0`
 MAILARRAY=("yahoo.com" "gmail.com" "rediffmail.com" "hotmail.com")
 CITYARRAY=("Delhi" "Mumbai" "Banglore" "Kolkata" "Chennai")
-#emailID=${MAILARRAY[$Index]}
-#cityID=${MAILARRAY[$Index]}
 COUNTER=0
 
    while [  $COUNTER -lt 100 ]; do
-   	 #divide=`expr $bignumber - 9999999999`
-     #telephone=`head -c 10 < (echo $bignumber)`
      telephone=$((7838000000+$RANDOM%9999999999))
      echo ${MAILARRAY[`expr $RANDOM % 4`]}__${CITYARRAY[`expr $RANDOM % 5`]}__$telephone>>$path/Subscribers.txt
      let COUNTER=COUNTER+1
--- ReportGeneration/SelectByCity.sh
+++ ReportGeneration/SelectByCity.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 path=`dirname $0`
-base=`basename $0`
-echo $base
-touch $path/YahooUsers.txt 
-fileName=$path/YahooUsers.txt
-echo $fileName      
-#sed -n '/yahoo.com/w $path/YahooUsers.txt' $path/Subscribers.txt
 grep -i 'Chennai' $path/Subscribers.txt > $path/ChennaiUsers.txt
 echo '--END--'
