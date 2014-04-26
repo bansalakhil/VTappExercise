eb1def87663014ac9f1c140ae2d2822e9f4fa2e6
shruti2791 <shruti@vinsol.com>
2014-04-26 15:55:19 +0530

    Exercise1

--- Javascript/Exercise1/exercise1.js
+++ Javascript/Exercise1/exercise1.js
@@ -7,12 +7,13 @@ function callme(){
 					if(this.age > otherUser.age){
             var result1 = this.name+" is older than "+otherUser.name;
             document.getElementById("result").innerHTML = result1;
+          }else{
             var result2 = otherUser.name+" is older than "+this.name;;
             document.getElementById("result").innerHTML = result2;
+          }
+            
 					}
 			}
-			}
-
 			var user1 = new User(document.getElementById('username1').value,document.getElementById('userage1').value);
 			var user2 = new User(document.getElementById('username2').value,document.getElementById('userage2').value);
 			user1.compare(user2);
